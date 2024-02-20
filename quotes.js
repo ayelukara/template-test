7af0dbe3f6ada473f633fdace6f8dffb9672d91d
ayelukara <tolakolap@gmail.com>
2024-02-20 01:51:24 +0100

    first commit

---
+++ quotes.js
@@ -0,0 +1,42 @@
+const localQuotes = [
+    {
+        text: 'Adaba o nani a n kun igbe, ino n jo eiye n lo, ketekete ku, iso pin.',
+        author: 'Yoruba Proverb',
+    },
+    {
+        text: 'Iponju ko je ki araiye so otito, aisotito ko je ki iponju fi araiye s\'ile.',
+        author: 'Olori Funke Ladipo',
+    },
+    {
+        text: 'Igba funfun rebete, agba to r\'ofo ika omo re yio je n\'ibe.',
+        author: 'Yoruba Proverb',
+    },
+    {
+        text: 'Araba ni baba, eni a ba l\'aba ni baba.',
+        author: 'Yoruba Proverb',
+    },
+    {
+        text: 'Ina n jo, ogiri o sa.',
+        author: 'Yoruba Proverb',
+    },
+    {
+        text: 'Bi ekolo ba j\'uba ile, o di dandan ki ile l\'anu.',
+        author: 'Yoruba Proverb',
+    },
+    {
+        text: 'Kii se ounje ni o je ki olongbo dabi, ekun.',
+        author: 'Yoruba Proverb',
+    },
+    {
+        text: 'Ohun ti eiye je ni eiye o gbe fo.',
+        author: 'Yoruba Proverb',
+    },
+    {
+        text: 'Gbà ní suki ni gbàlákàsù, àkàsù bàǹbà ni ẹnu agárán, b\'ẹ́nu agánrán bá gbé oko, àkàsù bàǹbà náà a gb\'ájà.',
+        author: 'Yoruba Proverb',
+    },
+    {
+        text: 'Gbígbó ajá kìí p\'ajá, kínkàn àgbò kìí p\'àgbò.',
+        author: 'Yoruba Proverb',
+    },
+]
